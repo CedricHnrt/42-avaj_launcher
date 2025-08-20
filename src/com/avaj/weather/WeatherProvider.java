d777c8fbce396235a3c92ce58c3c867ce075c42f
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-20 22:06:40 +0200

    feat(weather): enhance WeatherProvider to include tick-based weather variability

--- src/com/avaj/weather/WeatherProvider.java
+++ src/com/avaj/weather/WeatherProvider.java
@@ -5,6 +5,7 @@ import com.avaj.aircraft.Coordinates;
 public class WeatherProvider {
     private static WeatherProvider instance;
     private final String[] weather = {"SUN", "FOG", "RAIN", "SNOW"};
+    private int tick = 0;
 
     private WeatherProvider() {
     }
@@ -14,7 +15,11 @@ public class WeatherProvider {
     }
 
     public String getCurrentWeather(Coordinates p_coordinates) {
-        int seed = (p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight()) % weather.length;
+        int seed = (p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight() + this.tick) % weather.length;
+        this.tick++;
+        if (this.tick == Integer.MAX_VALUE) {
+            this.tick = 0;
+        }
         return weather[seed];
     }
 }
