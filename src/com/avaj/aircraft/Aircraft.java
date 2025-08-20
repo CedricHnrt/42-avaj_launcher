1372cf3e7a69202c05296c7687eb630c91dd2cec
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-20 21:13:40 +0200

    feat(aircraft): extend Aircraft class from Flyable and implement unique ID generation

--- src/com/avaj/aircraft/Aircraft.java
+++ src/com/avaj/aircraft/Aircraft.java
@@ -1,20 +1,30 @@
 package com.avaj.aircraft;
 
-public class Aircraft {
+public class Aircraft extends Flyable {
     protected long id;
-    protected String type;
     protected String name;
     protected Coordinates coordinates;
 
-    protected Aircraft(long id, String type, String name, Coordinates coordinates) {
-        this.id = id;
-        this.type = type;
-        this.name = name;
-        this.coordinates = coordinates;
+    private static long idCounter = 0;
+
+    protected Aircraft(long p_id, String p_name, Coordinates p_coordinate) {
+        this.id = p_id;
+        this.name = p_name;
+        this.coordinates = p_coordinate;
+    }
+
+    protected static long nextId() {
+        if (idCounter == Long.MAX_VALUE) {
+            throw new IllegalStateException("Error: Maximum number of aircraft reached");
+        }
+        return ++idCounter;
     }
 
     @Override
     public String toString() {
-        return this.type + "#" + this.name + "(" + this.id + ")";
+        return this.getClass().getSimpleName() + "#" + this.name + "(" + this.id + ")";
     }
+
+    @Override
+    public void updateConditions() {/*Useless but needed to respect diagram*/}
 }
