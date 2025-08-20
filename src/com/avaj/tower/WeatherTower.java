aa9d902924122dc031ce7b050ccca9f349396fa1
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-20 22:07:13 +0200

    feat(weather): implement weather retrieval logic in WeatherTower using WeatherProvider

--- src/com/avaj/tower/WeatherTower.java
+++ src/com/avaj/tower/WeatherTower.java
@@ -1,6 +1,11 @@
 package com.avaj.tower;
 
+import com.avaj.aircraft.Coordinates;
+import com.avaj.weather.WeatherProvider;
+
 public class WeatherTower  extends Tower {
-    public String getWeather() {return "WIP"; /*TODO: Implement weather retrieval logic*/}
+    public String getWeather(Coordinates p_coordinates) {
+        return WeatherProvider.getInstance().getCurrentWeather(p_coordinates);
+    }
     public void changeWeather() {}
 }
