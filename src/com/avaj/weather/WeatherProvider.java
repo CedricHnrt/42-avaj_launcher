d4cc067e6ebdff4a07ad1c47eb9c9f1aa4aad96d
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-21 16:36:15 +0200

    refactor(weather): simplify WeatherProvider constructor and fix tick reset condition

--- src/com/avaj/weather/WeatherProvider.java
+++ src/com/avaj/weather/WeatherProvider.java
@@ -7,8 +7,7 @@ public class WeatherProvider {
     private final String[] weather = {"SUN", "FOG", "RAIN", "SNOW"};
     private int tick = 0;
 
-    private WeatherProvider() {
-    }
+    private WeatherProvider() {}
 
     public static WeatherProvider getInstance() {
         return instance = instance == null ? new WeatherProvider() : instance;
@@ -17,7 +16,7 @@ public class WeatherProvider {
     public String getCurrentWeather(Coordinates p_coordinates) {
         int seed = (p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight() + this.tick) % weather.length;
         this.tick++;
-        if (this.tick == Integer.MAX_VALUE) {
+        if (this.tick == weather.length) {
             this.tick = 0;
         }
         return weather[seed];
