f105d1c73cdc2e111091246f8e6c64ebcc040822
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-21 16:37:17 +0200

    feat(aircraft): update newAircraft method to accept longitude, latitude, and height parameters

--- src/com/avaj/aircraft/AircraftFactory.java
+++ src/com/avaj/aircraft/AircraftFactory.java
@@ -5,7 +5,8 @@ public class AircraftFactory {
 
     private AircraftFactory() {}
 
-    public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+    public Flyable newAircraft(String p_type, String p_name, int p_longitude, int p_latitude, int p_height) {
+        Coordinates p_coordinates = new Coordinates(p_longitude, p_latitude,  p_height);
         return switch (p_type) {
             case "Helicopter" -> new Helicopter(Aircraft.nextId(), p_name, p_coordinates);
             case "JetPlane" -> new JetPlane(Aircraft.nextId(), p_name, p_coordinates);
