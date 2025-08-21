7ca6a41fa032459a94881ae6ce257fa1e397c101
Cédric Honorat <chonorat@student.42lyon.fr>
2025-08-21 16:36:31 +0200

    feat(weather): implement weather change notification in WeatherTower

--- src/com/avaj/tower/Tower.java
+++ src/com/avaj/tower/Tower.java
@@ -5,15 +5,19 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Tower {
-    private List<Flyable> observers = new ArrayList<>();
+    private final List<Flyable> observers = new ArrayList<>();
 
     public void register(Flyable p_flyable) {
         this.observers.add(p_flyable);
-        System.out.println("Tower says: " + p_flyable.toString() + "registered to weather tower.");
+        System.out.println("Tower says: " + p_flyable.toString() + " registered to weather tower.");
     }
     public void unregister(Flyable p_flyable) {
         this.observers.remove(p_flyable);
-        System.out.println("Tower says: " + p_flyable.toString() + "unregistered from weather tower.");
+        System.out.println("Tower says: " + p_flyable.toString() + " unregistered from weather tower.");
     }
-    protected void conditionChanged() {/*TODO: Implement weather condition changes*/}
+    protected void conditionChanged() {
+        for (Flyable p_flyable : observers) {
+            p_flyable.updateConditions();
+        };
+    }
 }
